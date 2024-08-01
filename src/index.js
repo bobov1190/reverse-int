63cf45e120158ae89a452b1dc8a78a4d62ed6baa
Bryan <bobov1190@gmail.com>
2024-08-01 12:36:48 +0500

    feat : have a solution

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 module.exports = function reverse(n) {
+    // If number is negative, i am making it positive
     if (n < 0) n *= -1;
+    // String - to make our number to typeof string
+    // split - to make an array
+    // reverse - to reverse the array
+    // join - to make it back to string and it is necessary to add "" into a ()
+    // Number - to make it back to number
     return Number(String(n).split("").reverse().join(""));
 };
